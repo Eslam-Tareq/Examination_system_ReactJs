ac6ef279e3a971d32a2eddf1a44d8c7f9d6ba54a
ITI-Student <student@iti.gov.eg>
2026-01-30 14:35:23 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white text-xl">
+      Structure جاهز 🔥
+    </div>
+  );
+}
+
+export default App;
